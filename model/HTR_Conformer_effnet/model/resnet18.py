109d69e86037dd47f264455735c8727e24035882
0xk0ry <phamtruc.main@gmail.com>
2025-10-27 21:30:05 +0700

    Refactor ResNet18 layer creation to handle BasicBlockEfn downsampling internally

--- model/HTR_Conformer_effnet/model/resnet18.py
+++ model/HTR_Conformer_effnet/model/resnet18.py
@@ -142,19 +142,26 @@ class ResNet18(nn.Module):
         self.layer3 = self._make_layer(BasicBlockEfn, nb_feat, 2, stride=2)
 
     def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion, eps=1e-05),
-            )
-
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
+        # BasicBlockEfn builds its own downsample/projection; do not pass a downsample module
+        if block is BasicBlockEfn:
+            layers.append(block(self.inplanes, planes, stride))
+        else:
+            downsample = None
+            if stride != 1 or self.inplanes != planes * block.expansion:
+                downsample = nn.Sequential(
+                    nn.Conv2d(self.inplanes, planes * block.expansion,
+                              kernel_size=1, stride=stride, bias=False),
+                    nn.BatchNorm2d(planes * block.expansion, eps=1e-05),
+                )
+            layers.append(block(self.inplanes, planes, stride, downsample))
+
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes, 1, None))
+            if block is BasicBlockEfn:
+                layers.append(block(self.inplanes, planes, 1))
+            else:
+                layers.append(block(self.inplanes, planes, 1, None))
 
         return nn.Sequential(*layers)
 
